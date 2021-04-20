30937c6f272570873876afaaf72fb1b397b8aad7
Eugene Pokoo-Aikins <eugene.pokoo-aikins@amalitech.org>
2021-04-20 20:02:15 +0000

    Initial Commit

---
+++ server/userprofile.js
@@ -0,0 +1,13 @@
+Meteor.publish("Post", function () {
+  return Post.find();
+});
+
+Accounts.onCreateUser(function (options, user) {
+  //make sure that the user has a profile object
+  user.profile = user.profile || {};
+
+  //initiate follow to keep track of who they follow
+  user.profile.follow = [];
+
+  return user;
+});
